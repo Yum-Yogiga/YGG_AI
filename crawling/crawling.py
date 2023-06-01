ddb2f20bb10e3a974f98fe8407fb49540d4bb1eb
HochangJin <87602338+Hochang-Jin@users.noreply.github.com>
2023-06-01 20:42:28 +0900

    feat/change crawling data

--- crawling/crawling.py
+++ crawling/crawling.py
@@ -2,10 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
 from urllib.parse import quote_plus
 from bs4 import BeautifulSoup
 import time
 import csv
+
 
 
 location_list = ['홍대','연남','상수','합정']
@@ -30,8 +32,16 @@ while True:
 service = Service(ChromeDriverManager().install())
 chromeOption = webdriver.ChromeOptions()
 chromeOption.add_experimental_option('detach', True)
-chromeOption.add_argument('headless')
+# chromeOption.add_argument('headless')
 driver = webdriver.Chrome(service = service, options = chromeOption)
+
+# 버튼 요소 존재 확인
+def check_exists(class_name):
+    try:
+        driver.find_element(by=By.CLASS_NAME, value='Tvx37')
+    except NoSuchElementException:
+        return False
+    return True
 
 
 item_list = []
@@ -55,12 +65,12 @@ count = 0
 # # linktxt.close()
 
 # csv file
-f = open('맛집.csv', 'a')
+f = open('맛집.csv', 'a',encoding='utf-8')
 csvWriter = csv.writer(f)
 # csv_list =[['가게 이름',1,'인원 수',2,'인원 수',3,'인원 수',4,'인원 수',5,'인원 수',6,'인원 수',7,'인원 수',8,'인원 수',9,'인원 수',10,'인원 수','링크']]
 csv_list = []
 # link traversal review data save
-for it in search_list[20:]:
+for it in search_list:
     # url = f'https://m.place.naver.com/restaurant/{it}/review/visitor'
     url = it
     print(count)
@@ -68,27 +78,28 @@ for it in search_list[20:]:
 
     driver.get(url)
     time.sleep(0.3)
-    button = driver.find_elements(by=By.CLASS_NAME,value='Tvx37')[-1]
-    button.click()
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    review_list = soup.select('.nWiXa')
-    keywordPerson_list = soup.select('.TwM9q')
-    title = soup.select_one('#_header').text
-    temp = [title]
+    if check_exists('Tvx37'):
+        button = driver.find_elements(by=By.CLASS_NAME,value='Tvx37')[-1]
+        button.click()
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        review_list = soup.select('.nWiXa')
+        keywordPerson_list = soup.select('.TwM9q')
+        title = soup.select_one('#_header').text
+        temp = [title]
 
-    # print(keywordPerson_list[0].text)
-    # for i,j in review_list, keywordPerson_list:
-    #     print(j.text)
-    #     temp.append(i.text)
-    for i in range(10):
-        temp.append(review_list[i].text.strip("\""))
-        temp.append(keywordPerson_list[i].text.strip('이 키워드를 선택한 인원'))
-    temp.append(f'https://map.naver.com/v5/entry/place/{it}?c=15,0,0,0,dh')
-    csv_list.append(temp)
-    if count % 10 == 0:
-        csvWriter.writerows(csv_list)
-        csv_list = []
+        # print(keywordPerson_list[0].text)
+        # for i,j in review_list, keywordPerson_list:
+        #     print(j.text)
+        #     temp.append(i.text)
+        for i in range(len(review_list)):
+            temp.append(review_list[i].text.strip("\""))
+            temp.append(keywordPerson_list[i].text.strip('이 키워드를 선택한 인원'))
+        temp.append(f'https://map.naver.com/v5/entry/place/{it}?c=15,0,0,0,dh')
+        csv_list.append(temp)
+        if count % 10 == 0:
+            csvWriter.writerows(csv_list)
+            csv_list = []
 
 csvWriter.writerows(csv_list)
 f.close()
