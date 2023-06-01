3dd1f8ab059f84ab3d19b65f873c7fb6bd825c56
hochangJin <hochang0115@gmail.com>
2023-06-01 21:22:21 +0900

    feat/change crawling data post processing
    링크 주소가 잘못 저장되어있어서 옳은 주소로 수정

--- crawling/csv_post_processing.py
+++ crawling/csv_post_processing.py
@@ -22,6 +22,14 @@ for i in inputList:
         while len(i) < 21:
             i.append('')
         i.append(link)
+    #get link data
+    stringTemp = i[-1]
+    #불필요한 문자열 제거
+    #https://map.naver.com/v5/entry/place/ 앞 37 문자 제거
+    #\n?c=15,0,0,0,dh 뒤 15문자 제거
+
+    stringTemp = stringTemp[37:-15]
+    i[-1]=stringTemp
 
 print(inputList[117])
 
