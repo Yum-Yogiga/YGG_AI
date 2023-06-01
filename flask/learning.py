27783fdfdd29039510d8909f2c0b215555d6679c
hochangJin <hochang0115@gmail.com>
2023-06-01 23:15:01 +0900

    style/learning.py style fix

--- flask/learning.py
+++ flask/learning.py
@@ -11,19 +11,20 @@ import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from tabulate import tabulate
 
+
 # 표 출력
 def print_df(df):
     print(tabulate(df, headers='keys', tablefmt='psql'))
 
 
 # 검색할 값 추가하기.
-
 def add_want_keyword(data, keywords):
     data = data.append(pd.Series(keywords, index=data.columns), ignore_index=True)
 
     # index를 보기 편하게 미리 지정한 idx로 바꿈. 추후에 idx가 식당 이름이 될 것으로 예상
     data = data.set_index(keys=['식당이름'],drop=True)
     return data
+
 
 # print(data)
 def get_restaurant(keywords):
@@ -44,6 +45,6 @@ def get_restaurant(keywords):
     # similarity_rate_df.head()
 
     # 유사도 상위 5개 추천
-    indexs = similarity_rate_df['나의식당'].sort_values(ascending=False)[:6]
+    indexes = similarity_rate_df['나의식당'].sort_values(ascending=False)[:6]
 
-    return indexs
+    return indexes
