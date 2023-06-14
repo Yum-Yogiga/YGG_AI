37aba672f9b64792cf38ca3289b5d186a4c98812
hochangJin <hochang0115@gmail.com>
2023-06-14 17:24:20 +0900

    feat/make new api call
    for chat-to-recommendation

--- flask/flask_app.py
+++ flask/flask_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
-from learning import get_restaurant, add_want_keyword
+from learning import get_restaurant, get_restaurant2
+from chat_api import chat_to_keyword
 
 app = Flask(__name__)
 
@@ -19,10 +20,17 @@ def model():
 
     return jsonify(result)
 
-# def main():
-#     app.run(host='127.0.0.1', port=80, debug=False)
-#
-# if __name__ == '__main__':
-#     main()
+@app.route('/chat', methods=['POST'])
+def chat():
+    if request.method == 'POST':
+        params = request.get_json()
+        chat = params['chat']
+        result = chat_to_keyword(chat)
+    list = ['나의식당']
+    for i in result:
+        list.append(i)
+    result = get_restaurant2(list)
+    result = result.to_dict()
+    return jsonify(result)
 
 app.run(debug=True)
