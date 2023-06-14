5414dc0a571826649a5be0f44d81937c6db8ee44
HochangJin <87602338+Hochang-Jin@users.noreply.github.com>
2023-06-14 14:53:45 +0900

    chore/Make new python file
    To make percentage.csv

---
+++ crawling/make_new_csv.py
@@ -0,0 +1,47 @@
+import csv
+
+input = open('input2.csv','r')
+output = open('output2.csv','w',encoding='utf-8',newline='')
+
+csvReader = csv.reader(input)
+csvWriter = csv.writer(output)
+
+inputList = []
+
+for line in csvReader:
+    inputList.append(line)
+
+keywordList=['음식이 맛있어요',
+              '친절해요',
+              '특별한 메뉴가 있어요',
+              '매장이 청결해요',
+              '재료가 신선해요',
+              '가성비가 좋아요',
+              '양이 많아요',
+              '인테리어가 멋져요',
+              '혼밥하기 좋아요']
+
+
+outputList = [['가게이름',
+              '음식이 맛있어요',
+              '친절해요',
+              '특별한 메뉴가 있어요',
+              '매장이 청결해요',
+              '재료가 신선해요',
+              '가성비가 좋아요',
+              '양이 많아요',
+              '인테리어가 멋져요',
+              '혼밥하기 좋아요',
+              '링크']]
+
+indexList = [1,3,5,7,9,11,13,15,17,19]
+
+for ilist in inputList[1:]:
+    templist = [ilist[0],0,0,0,0,0,0,0,0,0,ilist[-1]]   # 가게이름, 키워드 9개, 링크
+    for idx in indexList:
+        if ilist[idx] in keywordList:
+            index = keywordList.index(ilist[idx])       # keyword 종류 찾기
+            templist[index+1] = ilist[idx+1]            # 키워드 숫자 저장
+    outputList.append(templist)
+
+csvWriter.writerows(outputList)
---
+++ crawling/percentage.py
@@ -0,0 +1,44 @@
+import csv
+
+# file setting
+input = open('output2.csv','r',encoding='utf-8')
+output = open('output3.csv','w',encoding='utf-8',newline='')
+
+# csv reader/writer setting
+csvReader = csv.reader(input)
+csvWriter = csv.writer(output)
+
+# csv read
+inputList = []
+
+for line in csvReader:
+    inputList.append(line)
+
+# csv output list
+outputList = [['가게이름',
+              '음식이 맛있어요',
+              '친절해요',
+              '특별한 메뉴가 있어요',
+              '매장이 청결해요',
+              '재료가 신선해요',
+              '가성비가 좋아요',
+              '양이 많아요',
+              '인테리어가 멋져요',
+              '혼밥하기 좋아요',
+              '링크']]
+
+# read and calc each keyword's percentage
+for i in inputList[1:]:
+    tempList = [i[0], 0, 0, 0, 0, 0, 0, 0, 0, 0, i[-1]]  # 가게이름, 키워드 9개, 링크
+    numList = []
+    for j in range(1,10):
+        numList.append(int(i[j]))
+
+    total = sum(numList)
+    for j in range(1,10):
+        tempList[j] = round(int(i[j])/total,3)
+
+    outputList.append(tempList)
+
+# csv write
+csvWriter.writerows(outputList)
