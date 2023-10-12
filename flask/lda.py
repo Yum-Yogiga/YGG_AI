a808113576403d92727e6c9014a2d6aff690c121
HochangJin <87602338+Hochang-Jin@users.noreply.github.com>
2023-10-12 15:31:30 +0900

    YGG-148-/chore: update lda.py

--- flask/lda.py
+++ flask/lda.py
@@ -35,19 +35,28 @@ lda = LinearDiscriminantAnalysis(n_components=2)
 
 lda.fit(data_scaled, data.target)
 data_lda = lda.transform(data_scaled)
+
+my_res = [0.3,0.3,0.1,0.1,0.1,0.1,0.1,0.1,0.1]
+restaurant = lda.transform([my_res])
 print(data_lda.shape)
+
+pred = lda.predict([my_res])
+print(pred[0])
+print(km_model.cluster_centers_[pred[0]])
 
 lda_columns=['lda_component_1','lda_component_2']
 irisDF_lda = pd.DataFrame(data_lda,columns=lda_columns)
 irisDF_lda['target']=data.target
 
-markers=['^', 's', 'o', 'v', '<', '>', '*', '3', 'D']
+markers=['1','2','3','4','8','.','o','v','^','<','>']
 
-for i, marker in enumerate(markers):
+for i in range(k):
     x_axis_data = irisDF_lda[irisDF_lda['target']==i]['lda_component_1']
     y_axis_data = irisDF_lda[irisDF_lda['target']==i]['lda_component_2']
 
-    plt.scatter(x_axis_data, y_axis_data, marker=marker,label=i)
+    plt.scatter(x_axis_data, y_axis_data, marker='.',label=i)
+
+plt.scatter(restaurant[0][0],restaurant[0][1],marker='x',label='res')
 
 plt.legend(loc='upper right')
 plt.xlabel('lda_component_1')
